0f47d40893112652e08c582707716d7fee3d606d
xiangma4970 <35127485+xiangma4970@users.noreply.github.com>
2018-01-05 18:49:42 +0900

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -94,7 +94,7 @@ public class Main {
 	    ArrayList<String> output = new ArrayList<String>();
 	    while (rs.next()) {
 			Double bd = rs.getDouble("goods1Value__c");
-			if (bd == null) {
+			if (bd == 0) {
 				output.add("Herokuコネクトサンプル名：" + rs.getString("Name") + "　　　" + "商品１：" + rs.getString("goods1__c")+ "　　　" + "商品１価格：" + rs.getString("goods1Value__c"));
 			}
 			else {
