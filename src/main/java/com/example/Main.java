9d3de73500780e0dc0ca5e42fdc3d5e4b538f42c
xiangma4970 <35127485+xiangma4970@users.noreply.github.com>
2018-01-08 10:20:41 +0900

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -149,7 +149,7 @@ public class Main {
 					output.add("Herokuコネクトサンプル名：" + nullToEmpty(rs.getString("Name")) + "　　　" + "商品１：" + nullToEmpty(rs.getString("goods1__c"))+ "　　　" + "商品１価格：" + nullToEmpty(rs.getString("goods1Value__c")));
 				}
 				else {
-					bd = bd * 1.08;
+					bd = floor(bd * 1.08);
 					String bds = String.valueOf(bd);
 					output.add("Herokuコネクトサンプル名：" + nullToEmpty(rs.getString("Name")) + "　　　" + "商品１：" + nullToEmpty(rs.getString("goods1__c"))+ "　　　" + "商品１価格：" + nullToEmpty(rs.getString("goods1Value__c"))+ "　　　" + "商品１価格(税込)：" + bds);
 				}
