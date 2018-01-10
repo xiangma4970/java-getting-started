8a97d9068540c880fdf8d842a1bc995e2788626f
xiangma4970 <35127485+xiangma4970@users.noreply.github.com>
2018-01-10 22:29:31 +0900

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -33,7 +33,7 @@
 	import java.sql.Statement;
 	import java.util.ArrayList;
 	import java.util.Map;
-	import java.util.HashMap
+	import java.util.HashMap;
 	import org.springframework.stereotype.Controller;
 	import org.springframework.ui.Model;
 	import org.springframework.util.StringUtils;
