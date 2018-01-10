76bfb28a0ded6e481fe7c764db18507330741334
xiangma4970 <35127485+xiangma4970@users.noreply.github.com>
2018-01-10 22:01:18 +0900

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,198 +1,209 @@
-/*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+	/*
+	 * Copyright 2002-2014 the original author or authors.
+	 *
+	 * Licensed under the Apache License, Version 2.0 (the "License");
+	 * you may not use this file except in compliance with the License.
+	 * You may obtain a copy of the License at
+	 *
+	 *      http://www.apache.org/licenses/LICENSE-2.0
+	 *
+	 * Unless required by applicable law or agreed to in writing, software
+	 * distributed under the License is distributed on an "AS IS" BASIS,
+	 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	 * See the License for the specific language governing permissions and
+	 * limitations under the License.
+	 */
 
-package com.example;
+	package com.example;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+	import com.zaxxer.hikari.HikariConfig;
+	import com.zaxxer.hikari.HikariDataSource;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.beans.factory.annotation.Value;
+	import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+	import javax.sql.DataSource;
+	import java.sql.Connection;
+	import java.sql.ResultSet;
+	import java.sql.SQLException;
+	import java.sql.Statement;
+	import java.util.ArrayList;
+	import java.util.Map;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.ui.Model;
+	import org.springframework.util.StringUtils;
+	import org.springframework.web.bind.annotation.ModelAttribute;
+	import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.SearchForm;
+	import com.example.SearchForm;
 
-@Controller
-@SpringBootApplication
-public class Main {
+	@Controller
+	@SpringBootApplication
+	public class Main {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+	  @Value("${spring.datasource.url}")
+	  private String dbUrl;
 
-  @Autowired
-  private DataSource dataSource;
+	  @Autowired
+	  private DataSource dataSource;
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
-
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
-  }
-
-	@RequestMapping("/herokuConTest")
-	String herokuConTest(Map<String, Object> model){
-	  try (Connection connection = dataSource.getConnection()) {
-	    Statement stmt = connection.createStatement();
-	    ResultSet rs = stmt.executeQuery("SELECT * FROM salesforce.HerokuCon__c");
-
-	    ArrayList<String> output = new ArrayList<String>();
-	    while (rs.next()) {
-			Double bd = rs.getDouble("goods1Value__c");
-			if (bd == 0) {
-				output.add("Herokuコネクトサンプル名：" + rs.getString("Name") + "　　　" + "商品１：" + rs.getString("goods1__c")+ "　　　" + "商品１価格：" + rs.getString("goods1Value__c"));
-			}
-			else {
-				bd = bd * 1.08;
-				String bds = String.valueOf(bd);
-				output.add("Herokuコネクトサンプル名：" + rs.getString("Name") + "　　　" + "商品１：" + rs.getString("goods1__c")+ "　　　" + "商品１価格：" + rs.getString("goods1Value__c")+ "　　　" + "商品１価格(税込)：" + bds);
-			}
-	    }
-
-	    model.put("records", output);
-	    return "herokuConTest";
-	  } catch (Exception e) {
-	    model.put("message", e.getMessage());
-	    return "error";
+	  public static void main(String[] args) throws Exception {
+	    SpringApplication.run(Main.class, args);
 	  }
-	}
 
-	@RequestMapping("/search")
-	public String search(@ModelAttribute SearchForm form, Model model){
+	  @RequestMapping("/")
+	  String index() {
+	    return "index";
+	  }
 
-		String strShohin1 = form.getVal1();
-		String strShohin1val = form.getVal2();
-		
-		String sql = "SELECT * FROM salesforce.HerokuCon__c";
-		
-		if (!isNullOrEmpty(strShohin1) && !isNullOrEmpty(strShohin1val)) {
-			sql += " where goods1__c like " + "'%" + strShohin1 + "%'" + " and cast(goods1Value__c as varchar) like " + "'%" + strShohin1val + "%'";
-		}
-		else if (!isNullOrEmpty(strShohin1) && isNullOrEmpty(strShohin1val)) {
-			sql += " where goods1__c like " + "'%" + strShohin1 + "%'";
-		}
-		else if (isNullOrEmpty(strShohin1) && !isNullOrEmpty(strShohin1val)) {
-			sql += " where cast(goods1Value__c as varchar) like " + "'%" + strShohin1val + "%'";
-		}
-		
-		ArrayList<String> output = new ArrayList<String>();
+	  @RequestMapping("/db")
+	  String db(Map<String, Object> model) {
+	    try (Connection connection = dataSource.getConnection()) {
+	      Statement stmt = connection.createStatement();
+	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-		try (Connection connection = dataSource.getConnection()) {
+	      ArrayList<String> output = new ArrayList<String>();
+	      while (rs.next()) {
+	        output.add("Read from DB: " + rs.getTimestamp("tick"));
+	      }
+
+	      model.put("records", output);
+	      return "db";
+	    } catch (Exception e) {
+	      model.put("message", e.getMessage());
+	      return "error";
+	    }
+	  }
+
+	  @Bean
+	  public DataSource dataSource() throws SQLException {
+	    if (dbUrl == null || dbUrl.isEmpty()) {
+	      return new HikariDataSource();
+	    } else {
+	      HikariConfig config = new HikariConfig();
+	      config.setJdbcUrl(dbUrl);
+	      return new HikariDataSource(config);
+	    }
+	  }
+
+		@RequestMapping("/herokuConTest")
+		String herokuConTest(Map<String, Object> model){
+		  try (Connection connection = dataSource.getConnection()) {
 		    Statement stmt = connection.createStatement();
-		    ResultSet rs = stmt.executeQuery(sql);
+		    ResultSet rs = stmt.executeQuery("SELECT * FROM salesforce.HerokuCon__c");
 
+		    ArrayList<String> output = new ArrayList<String>();
 		    while (rs.next()) {
 				Double bd = rs.getDouble("goods1Value__c");
 				if (bd == 0) {
-					output.add("Herokuコネクトサンプル名：" + nullToEmpty(rs.getString("Name")) + "　　　" + "商品１：" + nullToEmpty(rs.getString("goods1__c"))+ "　　　" + "商品１価格：" + nullToEmpty(rs.getString("goods1Value__c")));
+					output.add("Herokuコネクトサンプル名：" + rs.getString("Name") + "　　　" + "商品１：" + rs.getString("goods1__c")+ "　　　" + "商品１価格：" + rs.getString("goods1Value__c"));
 				}
 				else {
 					bd = bd * 1.08;
-					bd = Math.floor(bd);
-					String bds = format(bd);
-					output.add("Herokuコネクトサンプル名：" + nullToEmpty(rs.getString("Name")) + "　　　" + "商品１：" + nullToEmpty(rs.getString("goods1__c"))+ "　　　" + "商品１価格：" + nullToEmpty(rs.getString("goods1Value__c"))+ "　　　" + "商品１価格(税込)：" + bds);
+					String bds = String.valueOf(bd);
+					output.add("Herokuコネクトサンプル名：" + rs.getString("Name") + "　　　" + "商品１：" + rs.getString("goods1__c")+ "　　　" + "商品１価格：" + rs.getString("goods1Value__c")+ "　　　" + "商品１価格(税込)：" + bds);
 				}
 		    }
 
+		    model.put("records", output);
+		    return "herokuConTest";
 		  } catch (Exception e) {
-		    output.add("DBerror" + sql);
+		    model.put("message", e.getMessage());
+		    return "error";
 		  }
+		}
 
-        //--------------------------------------
-        // 画面表示項目設定
-        //--------------------------------------
-        // 画面に渡す値を設定
-        model.addAttribute("form", form);
-        model.addAttribute("records", output);
+		@RequestMapping("/search")
+		public String search(@ModelAttribute SearchForm form, Model model){
 
-        // search画面を表示
-        return "search";
-    }
+			String strShohin1 = form.getVal1();
+			String strShohin1val = form.getVal2();
+			
+			String sql = "SELECT * FROM salesforce.HerokuCon__c";
+			
+			if (!isNullOrEmpty(strShohin1) && !isNullOrEmpty(strShohin1val)) {
+				sql += " where goods1__c like " + "'%" + strShohin1 + "%'" + " and cast(goods1Value__c as varchar) like " + "'%" + strShohin1val + "%'";
+			}
+			else if (!isNullOrEmpty(strShohin1) && isNullOrEmpty(strShohin1val)) {
+				sql += " where goods1__c like " + "'%" + strShohin1 + "%'";
+			}
+			else if (isNullOrEmpty(strShohin1) && !isNullOrEmpty(strShohin1val)) {
+				sql += " where cast(goods1Value__c as varchar) like " + "'%" + strShohin1val + "%'";
+			}
+			
+			ArrayList<Map<String,String>> output = new ArrayList<Map<String,String>>();
+			Map<String,String> outMap = new Map<String,String>();
 
-    private Boolean isNullOrEmpty(String val){
+			try (Connection connection = dataSource.getConnection()) {
+			    Statement stmt = connection.createStatement();
+			    ResultSet rs = stmt.executeQuery(sql);
 
-        if(null == val || "".equals(val)){
-            return true;
-        }
-        return false;
-    }
+			    while (rs.next()) {
+					Double bd = rs.getDouble("goods1Value__c");
+					if (bd == 0) {
+						outMap.put("Name",nullToEmpty(rs.getString("Name"));
+						outMap.put("goods1",nullToEmpty(rs.getString("goods1__c"));
+						outMap.put("goods1Value",nullToEmpty(rs.getString("goods1Value__c"));
+						
+						output.add(outMap);
+					}
+					else {
+						bd = bd * 1.08;
+						bd = Math.floor(bd);
+						String bds = format(bd);
+						
+						outMap.put("Name",nullToEmpty(rs.getString("Name"));
+						outMap.put("goods1",nullToEmpty(rs.getString("goods1__c"));
+						outMap.put("goods1Value",nullToEmpty(rs.getString("goods1Value__c"));
+						outMap.put("goods1Value",nullToEmpty(rs.getString("goods1Value__c"));
+						
+						output.add(outMap);
+					}
+			    }
 
-	private String nullToEmpty(String val){
-		
-        if(null == val){
-        	val = "";
-        }
-        return val;
-    }
+			  } catch (Exception e) {
+			    output.add("DBerror" + sql);
+			  }
 
-	private String format(double d){
-		
-		if(d == (long) d)
-		return String.format("%d",(int)d);
-		else
-		return String.format("%s",d);
+	        //--------------------------------------
+	        // 画面表示項目設定
+	        //--------------------------------------
+	        // 画面に渡す値を設定
+	        model.addAttribute("form", form);
+	        model.addAttribute("records", output);
+
+	        // search画面を表示
+	        return "search";
+	    }
+
+	    private Boolean isNullOrEmpty(String val){
+
+	        if(null == val || "".equals(val)){
+	            return true;
+	        }
+	        return false;
+	    }
+
+		private String nullToEmpty(String val){
+			
+	        if(null == val){
+	        	val = "";
+	        }
+	        return val;
+	    }
+
+		private String format(double d){
+			
+			if(d == (long) d)
+			return String.format("%d",(int)d);
+			else
+			return String.format("%s",d);
+		}
+
 	}
-
-}
