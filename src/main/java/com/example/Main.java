adec05b8da154ea1a8f7649358bc4932b679cb1b
xiangma4970 <35127485+xiangma4970@users.noreply.github.com>
2018-01-10 22:28:24 +0900

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -33,6 +33,7 @@
 	import java.sql.Statement;
 	import java.util.ArrayList;
 	import java.util.Map;
+	import java.util.HashMap
 	import org.springframework.stereotype.Controller;
 	import org.springframework.ui.Model;
 	import org.springframework.util.StringUtils;
@@ -168,7 +169,7 @@
 			    }
 
 			  } catch (Exception e) {
-			    output.add("DBerror" + sql);
+			    //output.add("DBerror" + sql);
 			  }
 
 	        //--------------------------------------
